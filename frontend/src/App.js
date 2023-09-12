35636842b35e0ca804727580a73685d236a01296
Atharva69669 <116868631+Atharva69669@users.noreply.github.com>
2023-09-12 15:38:07 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/signup" element={<SignUp/>} />
           <Route path="/login" element={<Login/>} />
+         <Route path="/dashboard" element={<Dashboard/>} />
         </Routes>
       </div>
     </Router>
