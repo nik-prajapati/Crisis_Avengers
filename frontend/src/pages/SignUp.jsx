f6ca40aae3486819e1713a773fb93392e9506d6b
Atharva69669 <116868631+Atharva69669@users.noreply.github.com>
2023-09-14 01:48:12 +0530

    Update SignUp.jsx

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
-import '../styles/Login.css';
-import axios from {axios};
+import '../styles/SignUp.css';
+import { useEffect } from 'react';
 
 const SignUp = () => {
     const [formData, setFormData] = useState({
         email: '',
         password: '',
+        name: '',
+        description: '',
+        phones: [],
+        loaction: '',
+        address: '',
     });
 
     const handleInputChange = (event) => {
@@ -16,9 +21,32 @@ const SignUp = () => {
         });
     };
 
+    const getCurrentLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const { latitude, longitude } = position.coords;
+                    const location = `Latitude: ${latitude}, Longitude: ${longitude}`;
+                    setFormData({
+                        ...formData,
+                        location: location,
+                    });
+                },
+                (error) => {
+                    console.error('Error getting location:', error);
+                }
+            );
+        } else {
+            console.error('Geolocation is not supported in this browser.');
+        }
+    };
+    useEffect(() => {
+        getCurrentLocation();
+    }, []);
+
 
     const handleSubmit = async (event) => {
-        event.preventDefault(); 
+        event.preventDefault();
         console.log('Form Data:', formData);
         try {
             const response = await axios.post('/api/signup', formData);
@@ -33,6 +61,31 @@ const SignUp = () => {
           }
     };
 
+    const handlePhoneInputChange = (event, index) => {
+        const newPhones = [...formData.phones];
+        newPhones[index] = event.target.value;
+
+        setFormData({
+            ...formData,
+            phones: newPhones,
+        });
+    };
+
+    const removePhoneInput = (index) => {
+        const newPhones = [...formData.phones];
+        newPhones.splice(index, 1);
+
+        setFormData({
+            ...formData,
+            phones: newPhones,
+        });
+    };
+    const addPhoneInput = () => {
+        setFormData({
+            ...formData,
+            phones: [...formData.phones, ''],
+        });
+    };
     return (
         <div className="outermain">
             <div className="main">
@@ -45,14 +98,24 @@ const SignUp = () => {
                             <p className="right-container__p">Enter your email and password to Sign Up</p>
                         </div>
                         <div className="input-container">
-                            <label htmlFor="email" className="right-container__label">Email</label>
+                            <label htmlFor="name" className="right-container__label">Name :</label>
                             <input
                                 type="text"
                                 className="right-container__input"
+                                name="name"
+                                placeholder="Name of your organisation"
+                                value={formData.name}
+                                onChange={handleInputChange}
+                            />
+
+                            <label htmlFor="email" className="right-container__label">Email id:</label>
+                            <input
+                                type="email"
+                                className="right-container__input"
                                 name="email"
-                                placeholder="Your email address"
+                                placeholder="Email id"
                                 value={formData.email}
-                                onChange={handleInputChange} 
+                                onChange={handleInputChange}
                             />
                             <label htmlFor="password" className="right-container__label">Password</label>
                             <input
@@ -61,11 +124,46 @@ const SignUp = () => {
                                 name="password"
                                 placeholder="Your password"
                                 value={formData.password}
-                                onChange={handleInputChange} 
+                                onChange={handleInputChange}
+                            />
+                            <div className="input-container">
+                            <label className="right-container__label">Phone Numbers:</label>
+                            {formData.phones.map((phone, index) => (
+                                <div key={index} className="phone-input">
+                                    <input
+                                        type="text"
+                                        className="right-container__input"
+                                        name={`phone-${index}`}
+                                        placeholder="Phone Number"
+                                        value={phone}
+                                        onChange={(e) => handlePhoneInputChange(e, index)}
+                                    />
+                                    <button type="button" onClick={() => removePhoneInput(index)}>Remove</button>
+                                </div>
+                            ))}
+                            <button type="button" onClick={addPhoneInput}>Add Phone Number</button>
+                        </div>
+                            <label htmlFor="address" className="right-container__label">Address:</label>
+                            <input
+                                type="text"
+                                className="right-container__input box"
+                                name="address"
+                                placeholder="Describe your organisation"
+                                value={formData.address}
+                                onChange={handleInputChange}
+                            />
+                            <label htmlFor="decription" className="right-container__label">Description:</label>
+                            <input
+                                type="text"
+                                className="right-container__input box"
+                                name="description"
+                                placeholder="Describe your organisation"
+                                value={formData.description}
+                                onChange={handleInputChange}
                             />
                         </div>
-                        <br/>
-                        <br/>
+                        <br />
+                        <br />
                         <button className="btn" onClick={handleSubmit}>SIGN UP</button>
                         <p className="right-container__bottom-text">Already have an account? <a href='/login'>Login</a></p>
                     </div>
