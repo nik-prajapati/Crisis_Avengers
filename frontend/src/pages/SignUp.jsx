00a49698ff972ad7edb9e9f00edec2f635f5a4cb
Atharva69669 <116868631+Atharva69669@users.noreply.github.com>
2023-09-13 15:10:16 +0530

    Update SignUp.jsx

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../styles/Login.css';
+import axios from {axios};
 
 const SignUp = () => {
     const [formData, setFormData] = useState({
@@ -16,9 +17,20 @@ const SignUp = () => {
     };
 
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault(); 
         console.log('Form Data:', formData);
+        try {
+            const response = await axios.post('/api/signup', formData);
+            if (response.data.error === false) {
+              console.log('Signup successful');
+            } else {
+              setError(response.data.message);
+            }
+          } catch (error) {
+            console.error('Error during signup:', error);
+            setError('An error occurred during signup.');
+          }
     };
 
     return (
@@ -29,8 +41,8 @@ const SignUp = () => {
                 <div className="right-container">
                     <div className="right-container__box">
                         <div className="right-container-box">
-                            <h2 className="right-container__h2">Welcome !</h2>
-                            <p className="right-container__p">Enter your email and password to sign Up</p>
+                            <h2 className="right-container__h2">Welcome to our Community</h2>
+                            <p className="right-container__p">Enter your email and password to Sign Up</p>
                         </div>
                         <div className="input-container">
                             <label htmlFor="email" className="right-container__label">Email</label>
@@ -40,7 +52,7 @@ const SignUp = () => {
                                 name="email"
                                 placeholder="Your email address"
                                 value={formData.email}
-                                onChange={handleInputChange} // Handle input change
+                                onChange={handleInputChange} 
                             />
                             <label htmlFor="password" className="right-container__label">Password</label>
                             <input
@@ -49,7 +61,7 @@ const SignUp = () => {
                                 name="password"
                                 placeholder="Your password"
                                 value={formData.password}
-                                onChange={handleInputChange} // Handle input change
+                                onChange={handleInputChange} 
                             />
                         </div>
                         <br/>
