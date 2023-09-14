3ae1618ffe9bc299b3e592f87a6a9c62bcfcd794
Atharva69669 <116868631+Atharva69669@users.noreply.github.com>
2023-09-14 22:43:13 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUp from './pages/SignUp';
-import Login from './pages/Login';
+import GovtLogin from './pages/GovtLogin';
 import Dashboard from './pages/Dashboard'
-
+import UpdateData from './pages/UpdateData';
+import RescueLogin from './pages/RescueLogin';
 function App() {
   return (
     <Router>
+    {/* <Navbar/> */}
       <div className="App">
         <Routes>
           <Route path="/signup" element={<SignUp/>} />
-          <Route path="/login" element={<Login/>} />
-         <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/rescuelogin" element={<RescueLogin/>} />
+          <Route path="/govtlogin" element={<GovtLogin/>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/update" element={<UpdateData/>} />
         </Routes>
       </div>
     </Router>
