1a1f5fa733a6953045514223b907dd6bafe2e342
Sarthak Gharat <sgharat0903@gmail.com>
2023-12-19 13:09:55 +0530

    Otp signup verification

--- frontend/src/pages/request/SideBar.jsx
+++ frontend/src/pages/request/SideBar.jsx
@@ -1,19 +1,43 @@
-import React from 'react'
-import './SideBar.scss'
-import GraphicEqIcon from '@mui/icons-material/GraphicEq';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import Groups2Icon from '@mui/icons-material/Groups2';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./SideBar.scss";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import Groups2Icon from "@mui/icons-material/Groups2";
+import { Link } from "react-router-dom";
 
 const SideBar = () => {
   return (
-    <div className='sidebar-container'>
-    <div className="access-btn"><GraphicEqIcon />Activity Records</div>
-    <div className="access-btn"><Link to='/resource' style={{ textDecoration: 'none', color: 'inherit' }}><AddShoppingCartIcon />Resource</Link></div>
-    <div className="access-btn"><Link to='/review' style={{ textDecoration: 'none', color: 'inherit' }}><NotificationsIcon />Review Requests</Link></div>
-    <div className="access-btn"><Link to='/request' style={{ textDecoration: 'none', color: 'inherit' }}><Groups2Icon />Collaborate</Link></div>
+    <div className="sidebar-container">
+      <div className="access-btn">
+        <GraphicEqIcon />
+        Activity Records
+      </div>
+      <div className="access-btn">
+        <Link
+          to="/resource"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <AddShoppingCartIcon />
+          Resource
+        </Link>
+      </div>
+      <div className="access-btn">
+        <Link to="/review" style={{ textDecoration: "none", color: "inherit" }}>
+          <NotificationsIcon />
+          Review Requests
+        </Link>
+      </div>
+      <div className="access-btn">
+        <Link
+          to="/request"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <Groups2Icon />
+          Collaborate
+        </Link>
+      </div>
     </div>
-  )
-}
-export default SideBar
+  );
+};
+export default SideBar;
