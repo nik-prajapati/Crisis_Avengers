e959b702de98a2a22697ef3620b116c03e729c7a
saadbelgi <114847994+saadbelgi@users.noreply.github.com>
2023-12-19 23:05:29 +0530

    23:05

--- backend/src/routes/getagencies.ts
+++ backend/src/routes/getagencies.ts
@@ -8,9 +8,6 @@ import Resource from '../models/resource';
 
 const router = Router();
 
-const API_KEY =
-  process.env.MAPQUEST_API_KEY || 'nuGdfaEudQgh4rlkNX49JgnTKbGnBBVm';
-
 // get all agencies within a particular radius (in meters)
 router.get('/', async (req, res) => {
   const { latitude, longitude, radius } = req.query;
@@ -37,66 +34,18 @@ router.get('/', async (req, res) => {
           spherical: true,
           distanceField: 'distance',
         },
-      }
+      },
+      {
+        $lookup: {
+          from: 'resources',
+          localField: '_id',
+          foreignField: 'agency_id',
+          as: 'resources',
+        },
+      },
     ]);
     console.log(agencies);
     res.send(agencies);
-    // console.log(agencies)
-    // console.log(agencies);
-    // agencies = agencies.filter((agency) => {
-    //   agency.location.latitude;
-    //   return (
-    //     haversineDistance(
-    //       { latitude: lat, longitude: long },
-    //       {
-    //         latitude: agency.location.latitude,
-    //         longitude: agency.location.longitude,
-    //       }
-    //     ) <= rad
-    //   );
-    // });
-    // console.log(agencies);
-    // const dist: number[] = [];
-    // for (let i = 0; i < agencies.length; i += 49) {
-    //   try {
-    //     const response = await axios.post(
-    //       `https://www.mapquestapi.com/directions/v2/routematrix?key=${API_KEY}`,
-    //       {
-    //         locations: [
-    //           `${latitude},${longitude}`,
-    //           ...(agencies
-    //             .slice(i, i + 49 < agencies.length ? i + 49 : undefined)
-    //             .map(
-    //               (agency) =>
-    //                 `${agency.location.latitute},${agency.location.longitude}`
-    //             )),
-    //         ],
-    //         options: {
-    //           manyToOne: true,
-    //         },
-    //       }
-    //     );
-    //     console.log(response.data);
-    //     dist.push(...response.data.distance.slice(1));
-    //     // console.log(response.data);
-    //   } catch (e) {
-    //     console.error(e);
-    //   }
-    // }
-
-
-    // const resources: any[] = [];
-    // for (let i = 0; i < agencies.length; i++) {
-    //   const x = await Resource.find({ agency_id: agencies[i]._id }).exec();
-    //   resources.push(x);
-    // }
-    // res.json({
-    //   error: false,
-    //   agencies: agencies.map((agency, idx) => {
-    //     return { ...agency, resources: resources[idx] };
-    //   }),
-    //   // .sort((a, b) => a.distance - b.distance),
-    // });
   }
 });
 
