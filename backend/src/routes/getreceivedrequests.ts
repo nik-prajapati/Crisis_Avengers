c530064cb399f71917234fab541eddfb105632af
Sarthak Gharat <sgharat0903@gmail.com>
2023-12-19 14:56:07 +0530

    accept reject req

--- backend/src/routes/getreceivedrequests.ts
+++ backend/src/routes/getreceivedrequests.ts
@@ -8,29 +8,31 @@ const router = Router();
 // get all agencies within a particular radius (in kilometers)
 router.get('/', isAuthenticated, isRescueAgency, async (req, res) => {
   if (!req.user) return;
-  const requests = await Request.find({requestee_id: req.user.id});
+  const requests = await Request.find({ requestee_id: req.user.id }).populate(
+    'govt_requester_id rescue_requester_id requestee_id'
+  );
   // const requests = await Request.aggregate([
   //   {
   //     $match: {
   //       requestee_id: req.user.id,
   //     },
   //   },
-    // {
-    //   $lookup: {
-    //     from: 'users',
-    //     localField: 'govt_requester_id',
-    //     foreignField: '_id',
-    //     as: 'govt_requester',
-    //   },
-    // },
-    // {
-    //   $lookup: {
-    //     from: 'users',
-    //     localField: 'rescue_requester_id',
-    //     foreignField: '_id',
-    //     as: 'rescue_requester',
-    //   },
-    // },
+  // {
+  //   $lookup: {
+  //     from: 'users',
+  //     localField: 'govt_requester_id',
+  //     foreignField: '_id',
+  //     as: 'govt_requester',
+  //   },
+  // },
+  // {
+  //   $lookup: {
+  //     from: 'users',
+  //     localField: 'rescue_requester_id',
+  //     foreignField: '_id',
+  //     as: 'rescue_requester',
+  //   },
+  // },
   // ]);
   // console.log(requests);
   res.json({ requests: requests });
