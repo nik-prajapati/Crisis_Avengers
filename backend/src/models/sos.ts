8b14a3f1cc0a737e09378fb2abf6bccb9c17e1e5
saadbelgi <114847994+saadbelgi@users.noreply.github.com>
2023-12-20 14:36:54 +0530

    14:36

--- backend/src/models/sos.ts
+++ backend/src/models/sos.ts
@@ -1,40 +1,45 @@
 import { Schema, model } from 'mongoose';
 import { Sos } from '../types/schema';
 
-const SosSchema = new Schema<Sos>({
-  typeOfDisaster: {
-    type: String,
-    enum: [
-      'Earthquakes',
-      'floods',
-      'Thunderstorm',
-      'Tornado',
-      'Cyclone',
-      'Industrial accident',
-      'Heatwave',
-      'Landslide',
-      'Forest fire',
-    ],
-    required: true,
-  },
-  location: {
-    type: {
+const SosSchema = new Schema<Sos>(
+  {
+    typeOfDisaster: {
       type: String,
-      enum: ['Point'],
+      enum: [
+        'Earthquakes',
+        'floods',
+        'Thunderstorm',
+        'Tornado',
+        'Cyclone',
+        'Industrial accident',
+        'Heatwave',
+        'Landslide',
+        'Forest fire',
+      ],
       required: true,
     },
-    coordinates: {
-      type: [Number],
-      required: true,
+    location: {
+      type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
     },
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    expires: '24h',
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
+SosSchema.index({ createdAt: 1 }, { expireAfterSeconds: 86400 });
 SosSchema.index({ location: '2dsphere' });
 
 const Sos = model<Sos>('Sos', SosSchema, 'sos');
